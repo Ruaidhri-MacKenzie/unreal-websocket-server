925207697f75b2b37ae065c4c1b3fd9b07304701
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2024-02-14 12:31:30 +0000

    Bound incoming data to text fields

--- public/main.js
+++ public/main.js
@@ -2,6 +2,10 @@ const HOST = "127.0.0.1";
 const PORT = 8080;
 const socket = new WebSocket(`ws://${HOST}:${PORT}`);
 
+const nameText = document.getElementById("name");
+const categoryText = document.getElementById("category");
+const postcodeText = document.getElementById("postcode");
+
 socket.addEventListener("open", (event) => {
 	console.log("Connected");
 });
@@ -15,5 +19,10 @@ socket.addEventListener("close", (event) => {
 });
 
 socket.addEventListener("message", (event) => {
-	console.log(event.data);
+	const data = JSON.parse(event.data);
+	console.log(data);
+
+	if (data.name) nameText.innerText = data.name;
+	if (data.category) categoryText.innerText = data.category;
+	if (data.postcode) postcodeText.innerText = data.postcode;
 });
